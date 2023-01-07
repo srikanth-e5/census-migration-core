d3a7a76cdf6f2de5ff57d8ba3fab0e369704e2ee
Sethuraman, Srikanth {DIST~Santa Clara} <srikanth.sethuraman@contractors.roche.com>
2023-01-07 08:22:19 +0530

    Base Setup of Census Migration Service

---
+++ src/main/java/com/org/census/CensusMigrationCoreApplication.java
@@ -0,0 +1,13 @@
+package com.org.census;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CensusMigrationCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CensusMigrationCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/census/CensusMigrationCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.census;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CensusMigrationCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
